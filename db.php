ec9f8c05892a9d9008a4382a9885e8e696b605cb
Guilherme Rufino <guilhermeliima74@gmail.com>
2017-06-09 17:08:32 -0300

    modificado

--- db.php
+++ db.php
@@ -3,3 +3,9 @@
 global $db;
 
 $db = new mysqli("localhost" , "root" , "123qwe" , "playList");
+
+
+if ($db->connect_errno) {
+   header("location:ops.php");
+}
+
---
+++ ops.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ERROR #407</title>
+</head>
+<body>
+	<h2> OPS! PARECE QUE VOCÊ NÃO ESTÁ CONECTADO AO BANCO DE DADOS OU HÁ ALGUM ERRO DE SINTAXE!</h2>
+	<a href="index.php">retornar</a>
+	<small>see in db.php</small>
+</body>
+</html>
