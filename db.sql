2bb7f4c2d89238033d1740540a760b4efdcbc3ef
Guilherme Rufino <guilhermeliima74@gmail.com>
2017-06-09 15:14:51 -0300

    a parada foi mudada

--- db.sql
+++ db.sql
@@ -2,4 +2,13 @@ CREATE DATABASE playlist;
 
 USE playlist;
 
-# Gui favor terminar a estrutura do DB.
+CREATE table playlist(
+id integer ,
+nome varchar(255),
+artista varchar(255),
+tipo varchar(255),
+primary key(id)
+);
+
+
+# Neri a estrutura do database foi alterada corretamente?
