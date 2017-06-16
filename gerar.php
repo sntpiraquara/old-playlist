c32aa5f8a8eb5c533049e0cfaa2c14ebf2ca354a
Guilherme Rufino <guilhermeliima74@gmail.com>
2017-06-16 19:08:21 -0300

    os problemas com a quantidade de musicas que o usuario quer selecionar foi resolvida!

--- gerar.php
+++ gerar.php
@@ -2,16 +2,22 @@
 include "include/all.php";
 
 
-	
-$numAgit = $_POST['agitada'];
-$numTrasnsi = $_POST['transicao'];
-$numAdor = $_POST['adoracao'];
+if (isset($_POST['gerar'])) {
 
+    $numAgit = $_POST['agitada'];
+    $numTrasnsi = $_POST['transicao'];
+    $numAdor = $_POST['adoracao'];
+    if (empty($numAgit) || empty($numTrasnsi) || empty($numAdor)) {
+        $_SESSION['empty'] = "esses campos não poder estar limpos!";
+        
+        redirect("index.php");
+    }
 
-$musica = new Musica;
+    $musica = new Musica;
 
-$_SESSION['musicas'] = $musica->gerar($numAgit,$numTrasnsi,$numAdor);
-redirect("index.php");
+    $_SESSION['musicas'] = $musica->gerar($numAgit,$numTrasnsi,$numAdor);
+    redirect("index.php");
+}
 
 
 if (isset($_POST['limpar'])) {
--- index.php
+++ index.php
@@ -8,6 +8,7 @@ if (!isset($_SESSION['musicas'])) {
     $_SESSION['musicas'] = [];
 }
 
+
 $musicas = [];
 $musicas = $_SESSION['musicas'];
 
@@ -35,12 +36,12 @@ $musicas = $_SESSION['musicas'];
         <div class="panel panel-default" style="margin-top: 22px;">
             <div class="panel-heading">Cadastrar Música</div>
             <div class="panel-body">
-            <?php if (isset($_SESSION['aviso'])): ?>
-                <div class="alert alert-danger">
-                    <?php echo $_SESSION['aviso']; ?>
-                    <?php unset($_SESSION['aviso']);?>
-                </div>
-            <?php endif;?>
+                <?php if (isset($_SESSION['aviso'])): ?>
+                    <div class="alert alert-danger">
+                        <?php echo $_SESSION['aviso']; ?>
+                        <?php unset($_SESSION['aviso']);?>
+                    </div>
+                <?php endif;?>
 
                 <form class="form" action="cadastrar.php" method="post">
                     <div class="form-group">
@@ -67,66 +68,78 @@ $musicas = $_SESSION['musicas'];
         <div class="panel panel-default">
             <div class="panel-heading">Gerar Playlist</div>
             <div class="panel-body">
-                <form action="gerar.php" method="post">
-                 <p>
-                    <input type="number" name="agitada" placeholder="agitada">
-                    <input type="number" name="transicao" placeholder="transição">
-                    <input type="number" name="adoracao" placeholder="adoração">
-                </p>
-                    <?php if (count($musicas) > 0): ?>
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Nome</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php foreach ($musicas as $musica) {?>
-                                <tr>
-                                    <td><?php echo $musica->nome; ?></td>
-                                </tr>
-                                <?php }?>
-                            </tbody>
-                        </table>
-                        <div class="form-group">
-                            <button class="btn btn-primary" name="gerar">Nova PlayList</button>
-                            <button class='btn btn-danger' name='limpar'>Limpar</button></p>
+                <form action="gerar.php" method="post" class="form-horizontal">
+                 <?php if (isset($_SESSION['empty'])): ?>
+                    <div class="alert alert-danger">
+                        <?php echo $_SESSION['empty']; ?>
+                        <?php unset($_SESSION['empty']);?>
+                    </div>
+                <?php endif;?>
+                    <p>
+                        <div class="col-sm-10">
+                          <input type="number" class="form-control" name="agitada" placeholder="Agitada">
                         </div>
-                    </form>
+                        <div class="col-sm-10">
+                          <input type="number" class="form-control" name="transicao" placeholder="Transição">
+                        </div>
+                        <div class="col-sm-10">
+                          <input type="number" class="form-control" name="adoracao"" placeholder="Adoração">
+                        </div>
+                  </p>
+                  <?php if (count($musicas) > 0): ?>
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php foreach ($musicas as $musica) {?>
+                            <tr>
+                                <td><?php echo $musica->nome; ?></td>
+                            </tr>
+                            <?php }?>
+                        </tbody>
+                    </table>
+                    <div class="form-group">
+                        <button class="btn btn-primary" name="gerar">Nova PlayList</button>
+                        <button class='btn btn-danger' name='limpar'>Limpar</button></p>
+                    </div>
                 <?php else: ?>
                     <button class="btn btn-primary btn-block" name="gerar">Gerar PlayList</button>
                 <?php endif;?>
-            </div>
+            </form>
         </div>
     </div>
-    <div class="col-xs-6">
+</div>
+<div class="col-xs-6">
 
-        <!-- tabela de musicas cadastradas -->
+    <!-- tabela de musicas cadastradas -->
 
-        <h3>Todas as Músicas</h3>
-        <table class="table table-bordered table-striped">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Nome</th>
-                    <th>Artista</th>
-                    <th>Tipo</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($todas as $all) {?>
-                <tr>
-                    <td><i class="fa fa-music"> </i></td>
-                    <td><?php echo $all->nome; ?></td>
-                    <td><i class="fa fa-user"></i> <?php echo $all->artista; ?></td>
-                    <td><?php echo $all->tipo; ?></td>
-                </tr>
-                <?php }?>
-            </tbody>
-        </table>
-    </div>
+    <h3>Todas as Músicas</h3>
+    <table class="table table-bordered table-striped">
+        <thead>
+            <tr>
+                <th></th>
+                <th>Nome</th>
+                <th>Artista</th>
+                <th>Tipo</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($todas as $all) {?>
+            <tr>
+                <td><i class="fa fa-music"> </i></td>
+                <td><?php echo $all->nome; ?></td>
+                <td><i class="fa fa-user"></i> <?php echo $all->artista; ?></td>
+                <td><?php echo $all->tipo; ?></td>
+            </tr>
+            <?php }?>
+        </tbody>
+    </table>
+</div>
 
-    <script src="assets/jquery/jquery-2.1.1.js"></script>
-    <script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="assets/jquery/jquery-2.1.1.js"></script>
+<script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
