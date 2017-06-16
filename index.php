df608af3d60e350932e32f77ae63acbcee634e1f
Guilherme Rufino <guilhermeliima74@gmail.com>
2017-06-16 19:42:10 -0300

    alteração no icone do artita da tabela geral

--- index.php
+++ index.php
@@ -69,48 +69,65 @@ $musicas = $_SESSION['musicas'];
             <div class="panel-heading">Gerar Playlist</div>
             <div class="panel-body">
                 <form action="gerar.php" method="post" class="form-horizontal">
-                 <?php if (isset($_SESSION['empty'])): ?>
+                   <?php if (isset($_SESSION['empty'])): ?>
                     <div class="alert alert-danger">
                         <?php echo $_SESSION['empty']; ?>
                         <?php unset($_SESSION['empty']);?>
                     </div>
-                <?php endif;?>
-                    <p>
-                        <div class="col-sm-10">
-                          <input type="number" class="form-control" name="agitada" placeholder="Agitada">
-                        </div>
-                        <div class="col-sm-10">
-                          <input type="number" class="form-control" name="transicao" placeholder="Transição">
-                        </div>
-                        <div class="col-sm-10">
-                          <input type="number" class="form-control" name="adoracao"" placeholder="Adoração">
-                        </div>
-                  </p>
-                  <?php if (count($musicas) > 0): ?>
-                    <table class="table table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Nome</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php foreach ($musicas as $musica) {?>
-                            <tr>
-                                <td><?php echo $musica->nome; ?></td>
-                            </tr>
-                            <?php }?>
-                        </tbody>
-                    </table>
-                    <div class="form-group">
-                        <button class="btn btn-primary" name="gerar">Nova PlayList</button>
-                        <button class='btn btn-danger' name='limpar'>Limpar</button></p>
-                    </div>
-                <?php else: ?>
-                    <button class="btn btn-primary btn-block" name="gerar">Gerar PlayList</button>
-                <?php endif;?>
-            </form>
-        </div>
+                    <?php endif;?>
+                <div class="form-group">    
+                    <label for="tipo1" class="col-sm-5 control-label">Agitada</label>
+                  <div class="col-sm-5">
+                      <input  
+                      type="number" 
+                      class="form-control" 
+                      name="agitada" 
+                      id="tipo1">
+                  </div>
+
+                  <label for="tipo2" class="col-sm-5 control-label">Transição</label>
+                  <div class="col-sm-5">
+                      <input  
+                      type="number" 
+                      class="form-control" 
+                      name="transicao" 
+                      id="tipo2"o">
+                  </div>
+
+                  <label for="tipo3" class="col-sm-5 control-label">Adoração</label>
+                  <div class="col-sm-5">
+                      <input  
+                      type="number" 
+                      class="form-control" 
+                      name="adoracao" 
+                      id="tipo3"">
+                  </div>
+              </div>
+              <?php if (count($musicas) > 0): ?>
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($musicas as $musica) {?>
+                        <tr>
+                            <td><?php echo $musica->nome; ?></td>
+                        </tr>
+                        <?php }?>
+                    </tbody>
+                </table>
+                <div class="form-group">
+                    <button class="btn btn-primary" name="gerar">Nova PlayList</button>
+                    <button class='btn btn-danger' name='limpar'>Limpar</button></p>
+                </div>
+            <?php else: ?>
+                <button class="btn btn-primary btn-block" name="gerar">Gerar PlayList</button>
+            <?php endif;?>
+        </form>
     </div>
+</div>
 </div>
 <div class="col-xs-6">
 
@@ -122,7 +139,7 @@ $musicas = $_SESSION['musicas'];
             <tr>
                 <th></th>
                 <th>Nome</th>
-                <th>Artista</th>
+                <th><i class="fa fa-user"></i> Artista</th>
                 <th>Tipo</th>
             </tr>
         </thead>
@@ -131,7 +148,7 @@ $musicas = $_SESSION['musicas'];
             <tr>
                 <td><i class="fa fa-music"> </i></td>
                 <td><?php echo $all->nome; ?></td>
-                <td><i class="fa fa-user"></i> <?php echo $all->artista; ?></td>
+                <td><?php echo $all->artista; ?></td>
                 <td><?php echo $all->tipo; ?></td>
             </tr>
             <?php }?>
