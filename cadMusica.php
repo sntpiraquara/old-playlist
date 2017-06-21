506384606a9ba3d35bd8d40905721012a5c63226
Big Boss <thya9o@outlook.com>
2017-06-21 01:30:38 -0300

    fix#001
    fix#001
    
    - Estruturação e validação do HTML

--- cadMusica.php
+++ cadMusica.php
@@ -1,32 +1,81 @@
-   <div class="col-xs-3">
+<!-- sumary -->
+   <!-- Thiago You -->
+   <!-- Branch: fix#001 -->
+   <!-- Mudanças: Validações e estruturação do HTML -->
+   <!-- Delete estes comentários de cabeçalho ou marcados com (delete) caso usem este branch -- >
+   <!-- Cuidado com a identação e não esqueça de fechar todas as divs -->
+   <!-- estruturei a grid e o layout sem conhecer qual a idéia conceitual que vcs possuem em mente e sem
+        nem testar, então não sei se realmente esta se comportanto como esperado
+   -->
+   <!-- tente evitar deixar muitos espaços em branco, muitos deles (muitos mesmo) podem afetar o desempenho -->
+   <!-- utilize a anotação ./ dentro de comentário quando necessário para indicar o fim de alguns blocos. Ajuda na leitura -->
+<!-- sumary -->
 
-        
-        <div class="panel panel-default" style="margin-top: 22px;">
-            <div class="panel-heading">Cadastrar Música</div>
-            <div class="panel-body">
-                <?php if (isset($_SESSION['aviso'])): ?>
-                    <div class="alert alert-danger">
-                        <?php echo $_SESSION['aviso']; ?>
-                        <?php unset($_SESSION['aviso']);?>
-                    </div>
-                <?php endif;?>
+<!-- row e col costumam andar juntas -->
+<div class="row">
+   <!-- Definindo o comportamento em diferentes grids torna a aplicação muito mais responsiva (delete) -->
+   <!-- O tamanho desejado éra mesmo 3/12? (delete) -->
+   <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+      <div class="panel panel-default">
+         <div class="panel-heading">
+            <!-- h1 é uma tag semantica e nao de tamanho, por isso a troca de size -->
+            <h1 class="font20">Cadastrar Música</h1>
+         </div>
+         <!-- ./panel-headin -->
+         <div class="panel-body">
+            
+            <?php if (isset($_SESSION['aviso'])): ?>
+               <div class="alert alert-danger">
+                  <!-- se o PHP estiver configurado é melhor utilizar as short tags que são mais performáticas (delete) -->
+                  <?php echo $_SESSION['aviso']; ?>
+                  <?php unset($_SESSION['aviso']); ?>
+               </div>
+            <?php endif; ?>
 
-                <form class="form" action="cadastrar.php" method="post">
-                    <div class="form-group">
-                        <input class="form-control" type="text" name="nome" placeholder="Nome">
-                    </div>
-                    <div class="form-group">
-                        <input class="form-control" type="text" name="artista" placeholder="Artista">
-                    </div>
-                    <div class="form-group">
-                        <select class="form-control" name="tipo">
-                            <option value="">Tipo de Música</option>
-                            <option value="agitada">Agitada</option>
-                            <option value="transicao">Transição</option>
-                            <option value="adoracao">Adoração</option>
+            <!-- Não esqueça de atribuir ID para elementos como inputs e outros que poderão ter interação com JS ou CSS (delete) -->
+            <!-- a seleção pelo ID é muito mais performática do que por outro atributo (delete) -->
+            <form id="submit-form" class="form" action="cadastrar.php" method="post">
+               <div class="row">
+                  <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
+                     <div class="form-group">
+                        <label for="nome">Nome:</label>
+                        <input id="nome" class="form-control" type="text" name="nome" placeholder="Nome">
+                     </div>
+                  </div>
+                  <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6">
+                     <div class="form-group">
+                        <label for="artista">Artista:</label>
+                        <input id="artista" class="form-control" type="text" name="artista" placeholder="Artista">
+                     </div>
+                  </div>
+               </div>
+               <!-- ./form-group-inputs -->
+               <div class="row">
+                  <div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+                     <div class="form-group">
+                        <label for="tipo">Tipo de Música:</label>
+                        <select id="tipo" class="form-control" name="tipo">
+                           <option value="" disabled selected>Selecione o Tipo</option>
+                           <option value="agitada">Agitada</option>
+                           <option value="transicao">Transição</option>
+                           <option value="adoracao">Adoração</option>
                         </select>
-                    </div>
-                    <button class="btn btn-block btn-primary" name="cadastrar">Cadastrar</button>
-                </form>
-            </div>
-        </div>
+                     </div>
+                  </div>
+               </div>
+               <!-- ./form-group-select -->
+               <!-- talvez esse button seja melhor posicionado em um panel footer (delete)-->
+               <div class="row">
+                  <div class="col-md-6 col-sm-6 col-xs-6 col-lg-6 col-md-offset-3 col-xs-offset-3 col-sm-offset-3 col-lg-offset-3">
+                     <button class="btn btn-block btn-primary btn-flat" name="cadastrar"><i class="fa fa-save"></i>&nbsp; Cadastrar</button>
+                  </div>
+               </div>
+            </form>
+         </div>
+         <!-- ./panel-body -->
+      </div>
+      <!-- ./panel -->
+   </div>
+   <!-- ./main-div -->
+</div>
+<!-- ./main-row -->
