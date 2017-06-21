c23cbd543a34b2fde99b15908dc33a47bc68b4e5
Neri Junior <neri@nerijunior.com>
2017-06-21 11:54:10 -0300

    usuarios init

--- db.sql
+++ db.sql
@@ -2,10 +2,18 @@ CREATE DATABASE playlist;
 
 USE playlist;
 
-CREATE TABLE musicas (
+CREATE TABLE IF NOT EXISTS musicas (
     id INTEGER AUTO_INCREMENT,
     nome VARCHAR(255),
     artista VARCHAR(255),
     tipo VARCHAR(255),
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS usuarios (
+    id INTEGER AUTO_INCREMENT,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    senha VARCHAR(255),
     PRIMARY KEY(id)
 );
